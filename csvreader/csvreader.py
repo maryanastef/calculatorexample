488438289380b15a4db389eabf30692b61f1df75
maryanastef <90362903+maryanastef@users.noreply.github.com>
2021-11-30 18:56:23 -0500

    updated branch 6

--- csvreader/csvreader.py
+++ csvreader/csvreader.py
@@ -7,14 +7,7 @@ class ReadCSV:
     def __init__(self,filepath):
         self.data=[]
 
-        with open(absolutepath(filepath)) as textdata:
-            csvdata=csv.DictReader(textdata, delimeter=",")
-            for row in csvdata:
-                self.data.append(row)
-        pass
-
-
-    def get_df_from_csv(self,filename)
+    def get_df_from_csv(self,addition_sample.csv):
     # return a data frame with method called csv reader
         return pd.read_csv("addition_sample.csv")
 
