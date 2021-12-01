a4d1e0efe36dd950c7142413061f765c4388bd93
maryanastef <90362903+maryanastef@users.noreply.github.com>
2021-12-01 12:33:55 -0500

    updated branch 6

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -6,6 +6,16 @@ import pandas as pd
 from calc.calculator import Calculator
 from calc.history.calculations import Calculations
 
+# use pandas to read csv file, loop through csv file, call assert on each calculation
+
+# setting a path
+directory = os.getcwd()
+path = os.path.dirname(directory)
+sys.path.append(path)
+
+# setting up current directory of files
+test_data = os.path.dirname(os.path.abspath(__file__)) + "/test_data/"
+
 
 @pytest.fixture
 def clear_history_fixture():
@@ -15,48 +25,40 @@ def clear_history_fixture():
 # You have to add the fixture function as a parameter to the test that you want to use it with
 
 
-def test_calculator_add_static(clear_history_fixture):
-    """testing that our calculator has a static method for addition"""
+def test_calculator_add(clear_history_fixture):
+    """testing the add function of the calculator"""
     # pylint: disable=unused-argument,redefined-outer-name
-    # using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0,2.0,5.0)
-    Calculator.add_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == 8.0
+    addition = pd.read_csv(test_data + "addition_sample.csv")
+    for item in range(len(addition)):
+        Calculator.add_numbers(addition.loc[item]["value_x"], addition.loc[item]["value_y"])
+        assert Calculator.get_last_result_value() == addition.loc[item]["result"]
 
 
-def test_calculator_subtract_static(clear_history_fixture):
-    """Testing the subtract method of the calc"""
+def test_calculator_subtract(clear_history_fixture):
+    """Testing the subtract function of the calculator"""
     # pylint: disable=unused-argument,redefined-outer-name
-    # using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0,2.0,3.0)
-    Calculator.subtract_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == -6.0
+    subtraction = pd.read_csv(test_data + "subtraction_sample.csv")
+    for item in range(len(subtraction)):
+        Calculator.subtract_numbers(subtraction.loc[item]["value_x"], subtraction.loc[item]["value_y"])
+        assert Calculator.get_last_result_value() == subtraction.loc[item]["result"]
 
 
 def test_calculator_multiply_static(clear_history_fixture):
-    """Testing the multiplication method of the calc"""
+    """Testing the multiplication function of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    # using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0,2.0,3.0)
-    Calculator.multiply_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == 6.0
+    multiplication = pd.read_csv(test_data + "multiplication_sample.csv")
+    for item in range(len(multiplication)):
+        Calculator.multiply_numbers(multiplication.loc[item]["value_x"], multiplication.loc[item]["value_y"])
+        assert Calculator.get_last_result_value() == multiplication.loc[item]["result"]
 
 
 def test_calculator_divide_static(clear_history_fixture):
-    """ Testing the division method of the calc"""
+    """ Testing the division function of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    # using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0, 1.0, 0.0)
-    Calculator.divide_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == "Cannot divide by zero"
-    my_tuple = (1.0, 1.0, 2.0)
-    Calculator.divide_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == 0.5
-
-    # use pandas to read csv file, loop through csv file, call assert on each calculation
-
-
-# setting a path
-directory = os.getcwd()
-path = os.path.dirname(directory)
-sys.path.append(path)
+    division = pd.read_csv(test_data + "division_sample.csv")
+    for item in range(len(division)):
+        if item == 0:
+            assert Calculator.get_last_result_value() == "Cannot divide by zero"
+        else:
+            Calculator.divide_numbers(division.loc[item]["value_x"], division.loc[item]["value_y"])
+            assert Calculator.get_last_result_value() == division.loc[item]["result"]
