c9c41c5d571aa8cf74147184684104ff03e0c763
maryanastef <90362903+maryanastef@users.noreply.github.com>
2021-12-02 15:41:45 -0500

    updated branch 6

--- tests/calculations_history_test.py
+++ tests/calculations_history_test.py
@@ -9,6 +9,7 @@ def clear_history_fixture():
     """define a function that will run each time you pass it to a test, it is called a fixture"""
     # pylint: disable=redefined-outer-name
     Calculations.clear_history()
+    return True
 
 
 @pytest.fixture
