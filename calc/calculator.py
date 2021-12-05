a19b2bd83a12e78dcfecbba1eb743cdbfa9580f5
maryanastef <90362903+maryanastef@users.noreply.github.com>
2021-12-05 00:08:49 -0500

    updating text to fix errors

--- calc/calculator.py
+++ calc/calculator.py
@@ -19,23 +19,23 @@ class Calculator:
     def add_numbers(tuple_values: tuple):
         """ adds list of numbers"""
         # create an addition object
-        Calculations.add_addition_calculation(tuple_values)
+        Calculations.add_calculation(tuple_values)
         return True
 
     @staticmethod
     def subtract_numbers(tuple_values: tuple):
         """ subtract a list of numbers from result"""
-        Calculations.add_subtraction_calculation(tuple_values)
+        Calculations.add_calculation(tuple_values)
         return True
 
     @staticmethod
     def multiply_numbers(tuple_values: tuple):
         """ multiplication number from result"""
-        Calculations.add_multiplication_calculation(tuple_values)
+        Calculations.add_calculation(tuple_values)
         return True
 
     @staticmethod
     def divide_numbers(tuple_values: tuple):
         """ division number from result"""
-        Calculations.add_division_calculation(tuple_values)
+        Calculations.add_calculation(tuple_values)
         return True
