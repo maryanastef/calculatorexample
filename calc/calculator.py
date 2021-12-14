bfd5191d18d8297c47050006e1352f9f60c9844f
maryanastef <90362903+maryanastef@users.noreply.github.com>
2021-12-13 22:23:31 -0500

    edited table

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,58 +1,54 @@
-""" Increment Function"""
-from idlelib import history
-
+""" This is the increment function"""
 from calc.history.calculations import Calculations
 
 
-# The calculator class just contains the methods to calculate
-
-
+# the calculator class just contains the methods to calculate
 class Calculator:
-    """ This is the Calculator Class"""
+    """ This is the Calculator class"""
 
-    # The calculator class just calls methods on Calculation Class
+    # the calculator class just calls methods on Calculations class
     @staticmethod
     def get_last_result_value():
-        """This is the gets the result of the calculation"""
-        # this method doesn't allow more than one action per function
+        """ This is the gets the result of the calculation"""
+        # I made this method so that I don't have more than one action per function
         return Calculations.get_last_calculation_result_value()
 
     @staticmethod
-    def add_numbers(tuple_values: tuple):
+    # tuple allows me to pass in as many values as a I want
+    def addition(tuple_values: tuple):
         """ adds list of numbers"""
-        # create an addition object
         Calculations.add_addition_calculation_to_history(tuple_values)
         return True
 
     @staticmethod
-    def subtract_numbers(tuple_values: tuple):
+    def subtraction(tuple_values: tuple):
         """ subtract a list of numbers from result"""
         Calculations.add_subtraction_calculation_to_history(tuple_values)
         return True
 
     @staticmethod
-    def multiply_numbers(tuple_values: tuple):
+    def multiplication(tuple_values: tuple):
         """ multiplication number from result"""
         Calculations.add_multiplication_calculation_to_history(tuple_values)
         return True
 
     @staticmethod
-    def divide_numbers(tuple_values: tuple):
+    def division(tuple_values:tuple):
         """ division number from result"""
         Calculations.add_division_calculation_to_history(tuple_values)
         return True
 
-    #   @staticmethod
-#   def get_history():
-#      """Get history """
-#     return Calculations.history()
+    @staticmethod
+    def getHistory():
+        """ Get history """
+        return Calculations.history
 
-#   @staticmethod
-#   def get_history_from_csv():
-#      """Get history from csv file"""
-#    return Calculations.read_history_from_csv()
+    @staticmethod
+    def getHistoryFromCSV():
+        """ Get history """
+        return Calculations.readHistoryFromCSV()
 
-# @staticmethod
-# def write_history_to_csv():
-#    """Write history """
-#    return Calculations.write_history_to_csv()
+    @staticmethod
+    def writeHistoryToCSV():
+        """ Get history """
+        return Calculations.writeHistoryToCSV()
--- calc/history/calculations.py
+++ calc/history/calculations.py
@@ -1,104 +1,78 @@
-""" Calculation History Class """
+"""Calculation history Class"""
 from calc.calculations.addition import Addition
 from calc.calculations.subtraction import Subtraction
 from calc.calculations.multiplication import Multiplication
-from calc.calculations.division import Division
-import pandas as pd
-
-
 class Calculations:
-    """ Calculations class manages the history of calculations"""
+    """Calculations class manages the history of calculations"""
     history = []
 
+    @staticmethod
+    def readHistoryFromCSV():
+        """Read the history from csv and put it into the history """
+   
+    @staticmethod
+    def writeHistoryToCSV():
+        """Write the history to csv file"""
     # pylint: disable=too-few-public-methods
-
-    # method to write history to a csv file
-
-    #   @staticmethod
-    #   def read_history_from_csv():
-    #     """Read history from csv and put it in history"""
-    #     data = pd.read_csv("addition_sample.csv")
-    #     return Calculations.history.append(data)
-
-    #  @staticmethod
-    #  def write_history_to_csv(history):
-    #      """Write history to csv"""
-    #     return history.to_csv("history_csv.csv")
-
+   
     @staticmethod
     def clear_history():
-        """Clear the history of the calculator"""
+        """clear the history of calculations"""
         Calculations.history.clear()
         return True
-
+   
     @staticmethod
-    def history_count():
-        """Get number of items in history"""
+    def count_history():
+        """get number of items in history"""
         return len(Calculations.history)
-
+    
     @staticmethod
     def get_last_calculation_object():
+        """get last calculation"""
         return Calculations.history[-1]
-
+    
     @staticmethod
     def get_last_calculation_result_value():
-        """Get last calculation"""
-        return Calculations.history[-1].get_result()
-
+        """get last calculation"""
+        calculation = Calculations.get_last_calculation_object()
+        return calculation.get_result()
+  
     @staticmethod
     def get_first_calculation():
-        """Get first calculation"""
-        return Calculations.history[0].get_result()
-
+        """get first calculation"""
+        return Calculations.history[0]
+   
     @staticmethod
     def get_calculation(num):
-        """get a specific calculation from history"""
+        """ get a specific calculation from history"""
         return Calculations.history[num]
-
+    
     @staticmethod
     def add_calculation(calculation):
-        """Get a generic calculation from history"""
+        """ get a generic calculation from history"""
         return Calculations.history.append(calculation)
-
+   
     @staticmethod
     def add_addition_calculation_to_history(values):
         """create an addition and add object to history using factory method create"""
         Calculations.add_calculation(Addition.create(values))
-        # Get the result of the calculation
+        #Get the result of the calculation
         return True
-
+    
     @staticmethod
     def add_subtraction_calculation_to_history(values):
         """create a subtraction object to history using factory method create"""
         Calculations.add_calculation(Subtraction.create(values))
         return True
-
+    
     @staticmethod
     def add_multiplication_calculation_to_history(values):
         """Add a multiplication object to history using factory method create"""
         Calculations.add_calculation(Multiplication.create(values))
         return True
-
+    
     @staticmethod
     def add_division_calculation_to_history(values):
-        """Add a multiplication object to history using factory method create"""
+        """Add a division object to history using factory method create"""
         Calculations.add_calculation(Division.create(values))
         return True
-
-    @staticmethod
-    def get_calc_result_history():
-        """This returns a list of results """
-        results = []
-        for item in Calculations.history:
-            results.append(item.get_result())
-        return results
-
-# retrieve back the history, send this to table
-# convert data frame into correct format to be printed
-# table data in a dictionary , method on history that returns back a dictionary
-#   @staticmethod
-#   def data_to_table():
-#     data_dict = {}
-#     for item in Calculations.history:
-#         data_dict[item] = str(item)
-#    return data_dict
